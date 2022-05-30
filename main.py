6f379b58df747a9c4e48ac73ab310e8c018955a9
Navie Prasad <106122757+20128@users.noreply.github.com>
2022-05-30 22:19:36 +0000

    I added multiple questions to it

---
+++ main.py
@@ -0,0 +1,81 @@
+def qtest():
+  print("What does Kia Ora mean? ")
+  print("1: Hello 2: Goodbye 3: Mr Earl 4: Hair Cut")
+  q1_ans = input("")
+  q1_ans = q1_ans.lower()
+
+  if q1_ans == "1" or q1_ans == "hello":
+    print("Correct")
+  else:
+    print("Incorrect")
+
+def q1():
+  print("What does ka pai mean?")
+  print("1: Well done 2: Go eat 3: Navie 4: Go home")
+  q1_ans = input("")
+  q1_ans = q1_ans.lower()
+
+  if q1_ans == "1" or q1_ans == "well done": 
+    print("correct")
+  else:
+    print("incorrect")
+
+def q2():
+  print("How do you say good morning in Te Reo?") 
+  print(" 1: Tena koe 2: buenos dias 3: John-eun achim 4: Morena")
+  q2_ans = input("")
+  q2_ans = q2_ans.lower()
+
+  if q2_ans == "4" or q2_ans == "hello":
+    print("correct")
+  else:
+    print("incorrect")
+
+def q3():
+  print("What color is kikorangi?")
+  print("1: Blue 2: green 3: Gold 4: Emerald")
+  q3_ans = input("")
+  q3_ans = q3_ans.lower()
+
+  if q3_ans == "1" or q3_ans == "blue": 
+   print("correct")
+  else:
+    print("incorrect")
+
+def q4():
+  print("what is green in te reo?")
+  print("1:Kakariki 2:Kowhai 3: Kikorangi 4: Mangu")
+  q4_ans = input("")
+  q4_ans = q4_ans.lower() 
+
+ if q4_ans == "1" or q4_ans == "blue":
+   print("correct")
+ else:
+   print("incorrect")
+
+def q5():
+  print("what does the Te Reo phrase Nau Mai Haere Mai mean?")
+  print("1: Welcome 2: What is your name? 3: Hi my name is 4: Do you know how to do this?")
+  q5_ans = input("")
+  q5_ans = q5_ans.lower():
+
+if q5_ans == "1" or "welcome":
+  print(correct)
+else: 
+  print("incorrect")
+
+def q6():
+  print("How do you say how are you in Te Reo?")
+  print("1: Kei te pehea koe 2: Haere ra 3: Kia makona 4: Ka kite Ano")
+  q6_ans = input("")
+  q6_ans = q6_ans.lower():
+
+if q6_ans == "1" or "kei te pehea koe":
+  print("correct")
+else:
+  print("incorrect")
+
+def q7():
+  print("How do you say New Zealand in te reo?")
+  print("1: Aotearoa 2: Nueva zelandia 3: zealandia 4: New Aotearoa")
+  q7_ans
---
+++ readme.md
@@ -0,0 +1,81 @@
+Create 5 Question to teach someone Te Reo
+
+MR Earl 
+
+QTest
+What does Kia Ora mean
+1: Hello
+2: Goodbye
+3: Mr Earl
+4: Hair Cut
+
+
+Q1
+What does ka pai mean?
+1: Well done
+2: Go eat 
+3: Navie 
+4: Go home
+
+Q2
+How do you say Good morning in Te Reo?
+1: Tena koe
+2: buenos dias
+3: John-eun achim 
+4: Morena 
+
+Q3
+What color is kikorangi?
+1: Blue
+2: green
+3: Gold
+4: Emerald 
+
+Q4 
+What is Green in Te Reo
+1:Kakariki
+2:Kowhai
+3: Kikorangi 
+4: Mangu
+
+Q5
+What does the Te Reo phrase Nau Mai Haere Mai mean
+1: Welcome
+2: What is your name?
+3: Hi my name is 
+4: Do you know how to do this?
+
+Q6
+How do you say how are you in Te Reo?
+1: Kei te pehea koe 
+2: Haere ra
+3: Kia makona
+4: Ka kite Ano
+
+Q7 
+How do you say New zealand in Te reo
+1: Aotearoa 
+2: Nueva zelandia 
+3: zealandia 
+4: New Aotearoa 
+
+Q8 
+What is the most famous greeting in Te Reo that is widely used across new zealand?
+1: Kia Ora 
+2: Tena Koe
+3: Tena koutou katoa 
+4: Kia ora tena koutou koe
+
+Q9
+What does ata marie mean?
+1: Good morning 
+2: Good afternoon
+3: Good night 
+4: Good evening 
+
+Q10
+What does Nga mihi mean?
+1: Congratulations
+2: Good work
+3: Good job
+4: well done
